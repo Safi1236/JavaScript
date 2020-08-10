db6cb6251720c3e1c0c80464805ecc683244826f
Safi Ullah <safi90655@gmail.com>
2020-08-10 11:36:16 +0500

    Archive script.js and created new script.js
    Refectored required field check

--- Pro01/script1.js
+++ Pro01/script1.js
@@ -25,25 +25,36 @@ function isValidEmail(email){
     return re.test(String(email).toLowerCase());
 }
 
-// Function to check if required fields hava data
-function checkRequired(inputArray){
-    inputArray.forEach(function(input){
-        if(input.value === ''){
-            showError(input,  `${getFieldId(input)} is required`);
-        }else{
-            showSuccess(input);
-        }
-    });
-}
-
-// Function to get the id of the field
-function getFieldId(input){
-    return input.id.charAt(0).toUpperCase() + input.id.slice(1);
-}
 // This is an EventListener for the form on script
 form.addEventListener('submit', function(e) {
     e.preventDefault();
 
-    checkRequired([username, email, password, password2]);
+    if(username.value === ''){
+       showError(username, 'Username is required');
+    } else{
+        showSuccess(username);
+    }
+
+    if(email.value === ''){
+        showError(email, 'Email is required');
+     } 
+     else if( !isValidEmail(email.value)){
+          showError(email, 'Email is invalid');  
+     }
+     else{
+         showSuccess(email);
+     }
+
+     if(password.value === ''){
+        showError(password, 'Password is required');
+     } else{
+         showSuccess(password);
+     }
+
+     if(password2.value === ''){
+        showError(password2, 'Password2 is required');
+     } else{
+         showSuccess(password2);
+     }
 
 })
