998f5fe9b42bed2e8d5eaad5cca518895c284511
Safi Ullah <safi90655@gmail.com>
2020-10-16 22:54:35 +0500

    Update script.js
    Added complete javascript for project 8.

--- Pro08/script.js
+++ Pro08/script.js
@@ -44,7 +44,45 @@ function searchMeal(e){
 }
 // Function to fetch meal data using teh meal id
 function getMealById(mealId){
-    fetch()
+    fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`)
+    .then( res => res.json)
+    .then(data => {
+        const meal = data.meals[0];
+        addMealToDOM(meal);
+    })
+}
+
+// Function to add a meal to DOM
+function addMealToDOM(meal){
+    const ingredients = [];
+    for(let i = 1; i <= 20; i++){
+        if(meal[`strIngredient${i}`]){
+            ingredients.push(`${meal[`strIngredient${i}`]} - ${meal[`strMeasure${i}`]}`);
+        }else{
+            break;
+        }
+    };
+
+    selectedMeal.innerHTML = `
+        <div class="selected-meal">
+        <h1>${meal.strMeal}</h1>
+        <img src="${meal.strMealThumb}" alt="${meal.strMeal}" />
+        <div class="selected-meal-info">
+         ${meal.strCategory ? `<p>${meal.strCategory}</p> `: '' }
+         ${meal.strArea ? `<p>${meal.strArea}</p> `: '' }
+
+        </div>
+        <div class="main">
+           <p>
+           ${meal.strInstructions}
+           </p>
+           <h2>Ingredients</h2>
+           <ul>
+           ${ingredients.map( ingredient => `<li>${ingredient}</li>` ).join('')}
+           </ul>
+        </div>
+        </div>
+    `;
 }
 
 // Event Listner
